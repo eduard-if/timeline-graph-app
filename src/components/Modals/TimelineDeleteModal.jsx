fbf99930a35c1cf8a8d24f718ed944e59f40b200
eduard-if <eduard-dev@proton.me>
2023-06-30 20:44:56 +0300

    Delete Toast

---
+++ src/components/Modals/TimelineDeleteModal.jsx
@@ -0,0 +1,79 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { Button, Col, Container, Form, Modal, Row } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { HiExclamationCircle } from 'react-icons/hi2';
+import { deleteTimeline } from '../../actions/timelineActions';
+import { PuffLoader } from 'react-spinners';
+
+
+const TimelineDeleteModal = ({ showDelete, handleCloseDelete, itemId, setShowDeleteToast }) => {
+    const dispatch = useDispatch();
+
+    // const isInitialMount = useRef(true);
+
+    const timelineList = useSelector(state => state.timelineList);
+    const { timelines } = timelineList;
+    const timeline = timelines.filter(timeline => timeline.id === itemId);
+    const { title, id } = timeline[0];
+
+    const timelineDelete = useSelector(state => state.timelineDelete);
+
+    const handleSubmitDelete = () => {
+        dispatch(deleteTimeline(id));
+        handleCloseDelete();
+        setShowDeleteToast(true);
+    };
+
+    // bugs in showing the loader, component rerenders after the timeline is deleted
+    // throws error here --->  const { title, id } = timeline[0];
+    // useEffect(() => {
+    //     if (isInitialMount.current) {
+    //         isInitialMount.current = false;
+    //     } else if (!timelineDelete.loading) {
+    //         handleCloseDelete();
+    //         setShowDeleteToast(true);
+    //     }
+    // }, [timelineDelete.loading]);
+
+    return (
+        <Modal show={showDelete} onHide={handleCloseDelete} scrollable='true' centered>
+            <Modal.Header closeButton className='bg-danger border-0 text-light shadow-sm'>
+                <Modal.Title  >
+                    <i className='bi bi-trash3-fill pe-2' ></i>
+                    Delete Timeline
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Container >
+                    <Row>
+                        <Col className='text-decoration-underline text-center'>
+                            Are you sure you want to delete this timeline?
+                        </Col>
+                    </Row>
+
+                    <Row>
+                        <Col className='fw-bold fs-4 text-center mt-3' >
+                            {title}
+                        </Col>
+                    </Row>
+                </Container>
+            </Modal.Body>
+            <Modal.Footer className='justify-content-center border-0' >
+                {timelineDelete.loading ? <PuffLoader color='#17141f' role='status' className='mx-auto my-3' speedMultiplier={2} />
+                    : (
+                        <>
+                            <Button variant='light' className='rounded shadow-sm' onClick={handleCloseDelete}>
+                                Cancel <i className='bi bi-x-lg' ></i>
+                            </Button>
+                            <Button variant='danger' className='rounded shadow-sm' onClick={handleSubmitDelete}>
+                                <HiExclamationCircle className='fs-5 align-top mt-1' /> Delete
+                            </Button>
+                        </>
+                    )}
+
+            </Modal.Footer>
+        </Modal>
+    );
+};
+
+export default TimelineDeleteModal;
---
+++ src/components/Toasts/DeleteTimelineToast.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect } from 'react';
+import { Toast, ToastContainer } from 'react-bootstrap';
+import { HiOutlineExclamationTriangle } from 'react-icons/hi2';
+import { BsRecycle } from 'react-icons/bs'
+import { useSelector } from 'react-redux';
+
+const DeleteTimelineToastToast = ({ showDeleteToast, setShowDeleteToast }) => {
+    const timelineDelete = useSelector(state => state.timelineDelete);
+    const { error, loading, timeline } = timelineDelete;
+
+    useEffect(() => {
+        console.log('timelineDelete', showDeleteToast)
+    }, [timelineDelete])
+
+    return (
+        <ToastContainer
+            className='mb-4 mx-auto'
+            style={{
+                zIndex: 2,
+                position: 'fixed',
+                bottom: 0,
+                left: '50%',
+                transform: 'translateX(-50%)',
+            }} >
+            <Toast
+                onClose={() => setShowDeleteToast(false)}
+                show={showDeleteToast}
+                delay={3000}
+                autohide
+                className={!error ? 'warningToast' : 'errorToast'}
+                style={{
+                    borderRadius: '10px',
+                }}>
+                <Toast.Header className='bg-transparent border-0' >
+                    {!error ? (
+                        <>
+                            <BsRecycle className='fs-4 me-2' />
+                            <span className='me-auto fw-light text-dark fs-5'>Timeline deleted</span>
+                        </>
+                    )
+                        : <>
+                            <HiOutlineExclamationTriangle className='fs-2 me-1 text-danger' />
+                            <span className='me-auto fw-light text-dark fs-5'>Something went wrong</span>
+                        </>
+                    }
+                </Toast.Header>
+                {error && (
+                    <Toast.Body className='bg-transparent  text-center rounded m-2 pt-1'>
+                        <span className='text-danger fw-bold' >{error}</span>
+                    </Toast.Body>
+                )}
+
+            </Toast>
+        </ToastContainer>
+    )
+}
+
+export default DeleteTimelineToastToast;
