1cae3da2225684f897735d0b4bb49569562fb5af
eduard-if <eduard-dev@proton.me>
2023-06-30 18:00:38 +0300

    Timeline delete reducer

--- src/store.js
+++ src/store.js
@@ -1,12 +1,13 @@
 import { legacy_createStore as createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { eventCreateReducer, eventDeleteReducer, eventUpdateReducer, timelineCreateReducer, timelineListReducer, timelineOpenReducer, timelineUpdateReducer } from './reducers/timelineReducers';
+import { eventCreateReducer, eventDeleteReducer, eventUpdateReducer, timelineCreateReducer, timelineDeleteReducer, timelineListReducer, timelineOpenReducer, timelineUpdateReducer } from './reducers/timelineReducers';
 
 const reducer = combineReducers({
     timelineList: timelineListReducer,
     timelineCreate: timelineCreateReducer,
     timelineUpdate: timelineUpdateReducer,
+    timelineDelete: timelineDeleteReducer,
     timelineOpen: timelineOpenReducer,
     eventCreate: eventCreateReducer,
     eventDelete: eventDeleteReducer,
