efa425de71907804efcc477179bd3a8a5a4a52cf
eduard-if <eduard-dev@proton.me>
2023-07-02 18:31:03 +0300

    Rounded border

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -3,7 +3,7 @@ import { Alert } from 'react-bootstrap';
 
 const Message = ({ variant, children }) => {
   return (
-    <Alert variant={variant} className='border-0 opacity-50 shadow-sm'>
+    <Alert variant={variant} className='border-0 rounded opacity-50 shadow-sm'>
       {children}
     </Alert>
   );
